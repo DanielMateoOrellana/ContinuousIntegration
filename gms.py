e97da2558f37e5da9ff45aded3584510f3b56db3
unknown <jsbaidal@espol.edu.ec>
2024-11-02 21:29:15 -0500

    commit gms

--- gms.py
+++ gms.py
@@ -1,2 +1,11 @@
 import unittest
 import unittest.mock as mock  # Import mock for patching input
+
+#Run tests
+if __name__ == "__main__":
+    unittest.main()
+
+# Run the system
+if __name__ == "__main__":
+    gym_system = GymMembership()
+    gym_system.run_program()
